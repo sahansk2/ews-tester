0982349d466431f3ceec5360075c8632d8f044f3
Sahan Kumarasinghe <sahansk2@illinois.edu>
2020-10-16 18:27:59 -0400

    Added support for color output in emails

--- README.md
+++ README.md
@@ -1,8 +1,10 @@
 # EWS Test-runner
-
+![Preview of EWS Tester Result](./ews-tester.png)
 For testing your code on UIUC's EWS without ssh'ing into it all the time.
 
 This code will send you an email whenever your code is done building. This is a very bare-bones lightweight solution to needing to run your code remotely.
 
-Instructions to come soon™.
+Because this runs on EWS, most of the programs this uses don't have to be installed manually. The only program you need to compile and install manually is [Aha](https://github.com/theZiz/aha), which is used to convert build output into nice HTML that you can read. 
+
+More detailed instructions to come soon™.
 
--- worktree-repo/auto-script.sh
+++ worktree-repo/auto-script.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# DO NOT INVOKE DIRECTLY. There are a lot of environment variables not set.
+
 LOG_FILE="$HOME/log.txt"
 HASH=$(git rev-parse --short HEAD)
 REPO=$(git config --get remote.origin.url | cut -d ":" -f2 | cut -d "." -f1)
@@ -17,23 +19,26 @@ cd $GIT_WORK_TREE
 
 
 log_message "Sourcing required env"
+export TERM=xterm-color
 source /etc/profile
 source $HOME/.bashrc
 source $HOME/.bash_profile
-
-
 # tr -d '[\015\200-\377]' < output-bin.txt > output.txt
 
 log_message "Running job"
 source ./build-job.cfg
-./build-job.sh > out.esc 2>&1 
+# ./build-job.sh > out.esc 2>&1 
+script -efq /dev/null -c "./build-job.sh" | aha -x > output.html
 ERROR_CODE=$?
-cat out.esc | sed 's/\x1b\[[0-9;]*m//g' > output.txt
-cat out.esc | aha > output.html
+# cat out.esc | sed 's/\x1b\[[0-9;]*m//g' > output.txt
+# cat out.esc | aha > output.html
 
 log_message "Sending mail"
 source gitmail.cfg
-mail -s "$REPO@$HASH: Build \"$BUILD_NAME\" completed with exit code: $ERROR_CODE" $RECIPIENT < ./output.txt
+mutt -e 'set content_type=text/html' \
+	-s "$REPO@$HASH: Build \"$BUILD_NAME\" exit code: $ERROR_CODE" \
+	"$RECIPIENT" < output.html
+# mail -s "$(echo -e \nContent-Type: text/html")" $RECIPIENT < ./output.html
 
 log_message "Deleting artifacts"
 rm -f output.txt output-bin.txt output.html
